bd0242fba6706de824e7388493165941f3a83fab
James Morrison <James.Morrison96@hotmail.co.uk>
2023-07-11 09:18:37 +0100

    first commit

---
+++ runner.py
@@ -0,0 +1,14 @@
+from student import CodeClanStudent
+
+student = CodeClanStudent("James", "e65")
+print(student.name, student.cohort)
+
+print(student.talk())
+
+print(student.say_fav_language("Python"))
+
+
+
+
+
+
---
+++ student.py
@@ -0,0 +1,16 @@
+class CodeClanStudent: 
+
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
+        # self.talk = talk
+        
+    def talk(self):
+        return "I can talk!"
+
+
+    def say_fav_language(self, language):
+        return "I love " + language 
+
+
+
